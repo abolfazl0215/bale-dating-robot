7b28e65a82e8083aafba3238c7d44cd647ce26ba
abolfazl0215 <83293551+abolfazl0215@users.noreply.github.com>
2026-06-01 16:34:19 +0330

    change mongoose url

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: 6338,
   MONGODB_URI:
-    // "mongodb+srv://xchat:Abolfazl021_@botdb.omatymd.mongodb.net/?appName=botdb",
-    "mongodb://root:iufWbfg4Or7YpwwI@services.irn9.chabokan.net:10949",
+    "mongodb+srv://xchat:Abolfazl021_@botdb.omatymd.mongodb.net/?appName=botdb",
+  // "mongodb://root:iufWbfg4Or7YpwwI@services.irn9.chabokan.net:10949",
   AGES: Array.from({ length: 63 }, (_, i) => 18 + i),
   PROVIDER_TOKEN: "WALLET-l5dCPuAvjRjSLcEk",
   BOT_INVITE_BASE:
