5cd85a62871b513f13cb54e2a215efc34bbd6f7b
abolfazl0215 <83293551+abolfazl0215@users.noreply.github.com>
2026-06-04 11:45:13 +0330

    change platform to telegram

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Local Bull worker for broadcast queue (same process as before)
-global.currentPlatform = "bale";
-// global.currentPlatform = "telegram";
+// global.currentPlatform = "bale";
+global.currentPlatform = "telegram";
 require("./messageToAllQueue.js");
 
 const bot = require("./utils/bot.js");
